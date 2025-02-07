8fc68482a26d141b1df9b8d82a36b0058d085646
herry <herrytsai@gis.tw>
2025-02-08 02:01:06 +0800

    🏗️Fixed deploy.yml.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/poker-calculator/', // 替換成您的儲存庫名稱
+  base: './',  // 使用相對路徑
 })
